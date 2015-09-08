3ba505ad6d93696c6f61c93c0cca8c4bdc260bd4
turif <turif@freemail.hu>
2015-09-08 10:24:26 +0200

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -3,7 +3,7 @@
 KAFKA_DIR=/kafka_2.10-0.8.2.1
 
 echo 'Starting Zookeeper'
-$KAFKA_DIR/bin/zookeeper-server-start.sh $KAFKA_DIR/config/zookeeper.properties >zookeeperconsole.log
+$KAFKA_DIR/bin/zookeeper-server-start.sh $KAFKA_DIR/config/zookeeper.properties >zookeeperconsole.log &
 
 echo 'Starting Kafka'
-$KAFKA_DIR/bin/kafka-server-start.sh $KAFKA_DIR/config/server.properties >kafkaconsole.log
+$KAFKA_DIR/bin/kafka-server-start.sh $KAFKA_DIR/config/server.properties >kafkaconsole.log &
